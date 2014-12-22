0dd39227f3eb25a1e5f2edef1303b4a502e34e6b
Shayol <Vorga88@gmail.com>
2014-12-22 22:33:25 +0400

    add pagination, counting users, company to strong params in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,12 @@ class UsersController < ApplicationController
 
   def index
     @q = User.search(params[:q])
-    @users = @q.result(distinct: true)
+    @users = @q.result(distinct: true).paginate(:page => params[:page], :per_page => 10)
+    @database = @users.count
   end
 
   private
     def user_params
-      params.require(:user).permit(:name, :description, :url)
+      params.require(:user).permit(:name, :company, :description, :url)
     end
 end
